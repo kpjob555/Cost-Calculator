e5df36fbaa33c8c95008c511f232da833b8edb83
MJDC <kpjob555@gmail.com>
2019-10-09 22:10:59 +0700

    Updates

---
+++ precache-manifest.7324e39766c71535228e576a27aff0fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9788d75dcad7cd4e367c0fab2f57f73",
+    "url": "/Cost-Calculator/index.html"
+  },
+  {
+    "revision": "a0a83d293061e9266793",
+    "url": "/Cost-Calculator/static/css/main.fc4539e9.chunk.css"
+  },
+  {
+    "revision": "0af5beffdf13c485fdbf",
+    "url": "/Cost-Calculator/static/js/2.6e09d7fb.chunk.js"
+  },
+  {
+    "revision": "a0a83d293061e9266793",
+    "url": "/Cost-Calculator/static/js/main.adc4fb98.chunk.js"
+  },
+  {
+    "revision": "c89f13d25e088d6a9a01",
+    "url": "/Cost-Calculator/static/js/runtime-main.97d0a711.js"
+  }
+]);
