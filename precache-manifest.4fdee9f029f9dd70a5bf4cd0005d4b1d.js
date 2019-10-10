39ac531d4ddbc376939e53c9464a327306012805
MJDC <kpjob555@gmail.com>
2019-10-10 20:26:47 +0700

    Updates

--- precache-manifest.4fdee9f029f9dd70a5bf4cd0005d4b1d.js
+++ precache-manifest.4fdee9f029f9dd70a5bf4cd0005d4b1d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "28662535b200e5495becc9331cd1d472",
+    "revision": "4d2b07690ba5d4f49cf3c22925549173",
     "url": "/Cost-Calculator/index.html"
   },
   {
-    "revision": "e3065ad0abfad73c651f",
-    "url": "/Cost-Calculator/static/css/main.0edecebb.chunk.css"
+    "revision": "7f4267688d9571d8a1a0",
+    "url": "/Cost-Calculator/static/css/main.0dce1f18.chunk.css"
   },
   {
     "revision": "7012528a5c9c4656bcfa",
     "url": "/Cost-Calculator/static/js/2.d53c73d7.chunk.js"
   },
   {
-    "revision": "e3065ad0abfad73c651f",
-    "url": "/Cost-Calculator/static/js/main.f331aa7f.chunk.js"
+    "revision": "7f4267688d9571d8a1a0",
+    "url": "/Cost-Calculator/static/js/main.8cc330db.chunk.js"
   },
   {
     "revision": "c89f13d25e088d6a9a01",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Cost-Calculator/precache-manifest.8723af5b23ec469ecc2e8ad7636161e6.js"
+  "/Cost-Calculator/precache-manifest.4fdee9f029f9dd70a5bf4cd0005d4b1d.js"
 );
 
 self.addEventListener('message', (event) => {
